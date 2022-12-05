e089dc532fbf7d7a742cd2dccf9d589218bbfed9
Irina <irina@theluky.com>
2022-12-05 18:01:25 -0500

    Add stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Die from "./Die";
+import Statistics from "./Statistics";
 import { nanoid } from "nanoid";
 import { useState } from 'react';
 import reactLogo from './assets/react.svg'
@@ -7,15 +8,58 @@ import reactLogo from './assets/react.svg'
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
   const [tenzies, setTenzies] = React.useState(false);
+  const [rolls, setRolls] = React.useState(0);
+  const [time, setTime] = React.useState(0);
+  const [isActiveTimer, setIsActiveTimer] = React.useState(false);
+  const [bestResult, setBestResult] = React.useState(() => JSON.parse(localStorage.getItem("bestResult")) || []);
+  const [currentResult, setCurrentResult] = React.useState(() => JSON.parse(localStorage.getItem("currentResult")) || []);
 
   React.useEffect(() => {
     const allHeld = dice.every(die => die.isHeld);
     const firstValue = dice[0].value;
     const allSameValue = dice.every(die => die.value === firstValue);
+    const result = {
+      rolls: rolls,
+      time: time
+    }
     if (allHeld && allSameValue) {
-      setTenzies(true)
+      setTenzies(true);
+      setTime(0);
+      setIsActiveTimer(false);
+      setRolls(0);
+      setCurrentResult(result);
+      checkBest(result);
     };
   }, [dice]);
+
+  React.useEffect(() => {
+    let interval = null;
+  
+    if (isActiveTimer) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActiveTimer]);
+
+  React.useEffect(() => {
+    localStorage.setItem("bestResult", JSON.stringify(bestResult))
+  }, [bestResult])
+
+  React.useEffect(() => {
+    localStorage.setItem("currentResult", JSON.stringify(currentResult))
+  }, [currentResult])
+
+  function checkBest(result) {
+    if (!bestResult.time || bestResult.time > result.time) {
+      setBestResult(result)
+    } 
+  }
 
   function generateNewDie() {
     return {
@@ -35,6 +79,7 @@ function App() {
 
   function rollDice() {
     if (!tenzies) {
+      setRolls(oldRoll => oldRoll+1);
       setDice(oldDice => oldDice.map(die => {
         return die.isHeld ? die : generateNewDie()
       }));
@@ -45,6 +90,9 @@ function App() {
   };
 
   function holdDice(id) {
+    if (isActiveTimer === false) {
+      setIsActiveTimer(true)
+    }
     setDice(oldDice => oldDice.map(die => {
       return die.id === id ? {...die, isHeld: !die.isHeld} : die
     }));
@@ -66,6 +114,12 @@ function App() {
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+      <Statistics 
+        currentTime={currentResult.time}
+        currentRolls={currentResult.rolls}
+        bestTime={bestResult.time}
+        bestRolls={bestResult.rolls}
+      />
     </main>
   )
 
---
+++ src/Statistics.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Statistics(props) {
+    const {currentTime, currentRolls, bestTime, bestRolls} = props;
+
+     const currentMinutes = Math.floor((currentTime / 60000) % 60)
+    const currentSeconds = Math.floor((currentTime / 1000) % 60)
+
+    const bestMinutes = Math.floor((bestTime / 60000) % 60)
+    const bestSeconds = Math.floor((bestTime / 1000) % 60)
+
+    return(
+        <div>
+            {currentTime && bestTime ? 
+            <p className='stats'><div className='result'><span>CURRENT RESULT.</span> <span>{currentMinutes} {currentMinutes == 1 ? "minute" : "minutes"},</span> <span>{currentSeconds} {currentSeconds == 1 ? "second" : "seconds"},</span> <span>{currentRolls} {currentRolls == 1 ? "roll" : "rolls"}</span></div>
+            <div className='result'><span>BEST RESULT.</span> <span>{bestMinutes} {bestMinutes == 1 ? "minute" : "minutes"},</span> <span>{bestSeconds} {bestSeconds == 1 ? "second" : "seconds"},</span> <span>{bestRolls} {bestRolls == 1 ? "roll" : "rolls"}</span></div></p> :
+            <div className='no-result'>Play to get your results!</div>
+            }
+        </div>
+    )
+}
+
+export default Statistics
